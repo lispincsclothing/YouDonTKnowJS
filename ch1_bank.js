00b88329118d36275ef5f704c73d2451bfc8f63c
lispincsclothing <scullingnull+1@gmail.com>
2016-04-03 23:04:15 -0700

    Adding functions and loop to spend money, as well as condition to see if you can afford

--- ch1_bank.js
+++ ch1_bank.js
@@ -7,8 +7,36 @@
 
 const SPENDING_THRESHOLD = 200;
 const TAX_RATE = 0.08;
-const PHONE_PRIME = 99.99;
+const PHONE_PRICE = 99.99;
 const ACCESSORY_PRICE = 9.99;
 
 var bank_balance = 303.91;
 var amount = 0;
+
+function calculateTax(amount) {
+  return amount + TAX_RATE;
+}
+
+function formatAmount(amount) {
+  return '$' + amount.toFixed(2);
+}
+
+// loop to enable repeat purchases while bank_balance is above 0
+while (amount < bank_balance) {
+    amount += PHONE_PRICE;
+
+    if (amount < SPENDING_THRESHOLD) {
+        amount += ACCESSORY_PRICE;
+    }
+}
+
+// assuming standard tax rate
+amount += calculateTax(amount);
+
+console.log('Your purchase: ' + formatAmount(amount));
+
+//can you actually afford purchase?
+if (amount > bank_balance) {
+  // no you can't 
+    console.log('You can\'t afford this purchase');
+}
